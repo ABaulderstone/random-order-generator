8eab009cde920a4d7e03a0dd064f7eb60f19c9cd
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2020-12-04 10:25:59 +1030

    created Group class and spec, wrote attribute tests for Group, created test-group file

---
+++ group.rb
@@ -0,0 +1,12 @@
+class Group
+    attr_reader :name, :names_array
+    def initialize(name,file_path)
+        @name = name
+        @names_array = []
+        @file_path = file_path
+    end
+
+    
+    
+
+end
---
+++ spec/group_spec.rb
@@ -0,0 +1,26 @@
+require_relative ('../group')
+
+describe 'group' do 
+    before(:each) do 
+        @group = Group.new("Test group") 
+    end 
+    
+    it 'should be an instance of a Group' do 
+        expect(@group).to be_a Group
+    end
+    
+    it 'should initialize with a name' do 
+        expect(@group.name).to eq("Test group") 
+    end
+    
+    it 'should have an array of names attribute' do
+        expect(@group.names_array).to be_an_instance_of(Array)
+    end
+
+    it 'should have a file_path as a String' do
+        expect(@group.names_array).to be_an_instance_of(String)
+    end
+
+
+
+end 
